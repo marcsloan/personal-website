ed601a9a9895c5686f0339cff8b2fcfecacad4fa
Marc Sloan <marc.sloan.10@ucl.ac.uk>
2020-02-10 17:53:09 +0000

    evenly spaced homepage and made arrow smaller

--- frontend/src/components/index/IndexContainer.js
+++ frontend/src/components/index/IndexContainer.js
@@ -18,25 +18,35 @@ class IndexContainer extends Component {
     render() {
 
         let bottomHalfOfPage = (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-6 col-md-offset-2 text-justified">
-                        <p>To build it I spent 4 years developing, <a
-                            href="https://www.crunchbase.com/organization/context-scout" target="_blank">fundraising</a>,
-                            hiring, firing, testing and designing. I’ve written extensively about what went right and what
-                            went wrong, and what I learned from it. Have a read, I hope you can learn something too.</p>
-                        <div className="small-medium-spacer"/>
-                        <a href="http://blog.marcsloan.ai/" target="_blank" className="blog-button-link">
-                            <div className="blog-button">
-                                Go to Blog
+            <div className="row second-half">
+                <div className="col-3"></div>
+                <div className="col-8">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-md-12 text-justified">
+                                <p>To build it I spent 4 years developing, <a
+                                    href="https://www.crunchbase.com/organization/context-scout"
+                                    target="_blank">fundraising</a>,
+                                    hiring, firing, testing and designing. I’ve written extensively about what went
+                                    right and what
+                                    went wrong, and what I learned from it. Have a read, I hope you can learn something
+                                    too.</p>
+                                <div className="small-medium-spacer"/>
+                                <a href="http://blog.marcsloan.ai/" target="_blank" className="blog-button-link">
+                                    <div className="blog-button">
+                                        Go to Blog
+                                    </div>
+                                </a>
+                                <div className="small-medium-spacer"/>
+                                <p>The concept sprung from my 5 years of research into dynamic information retrieval at
+                                    UCL and
+                                    Microsoft Research, and I started working on it with Andrew O’Harney who I met on
+                                    Entrepreneur
+                                    First. You should also <a href="http://andrewoharney.com/" target="_blank">check him
+                                        out</a> too, he’s great. </p>
+                                <div className="small-medium-spacer"/>
                             </div>
-                        </a>
-                        <div className="small-medium-spacer"/>
-                        <p>The concept sprung from my 5 years of research into dynamic information retrieval at UCL and
-                            Microsoft Research, and I started working on it with Andrew O’Harney who I met on Entrepreneur
-                            First. You should also <a href="http://andrewoharney.com/" target="_blank">check him
-                                out</a> too, he’s great. </p>
-                        <div className="small-medium-spacer"/>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -53,7 +63,7 @@ class IndexContainer extends Component {
                             <div className="row">
                                 <div className="col-3 text-right">
                                     <img className="index-message-avatar"
-                                     src={this.props.global.STATIC_URL + "img/index/avatar-white.png"}/>
+                                         src={this.props.global.STATIC_URL + "img/index/avatar-white.png"}/>
                                 </div>
                                 <div className="col-8">
                                     <div className="index-message-box justify-content-center">
@@ -61,7 +71,7 @@ class IndexContainer extends Component {
                                             <span>Hi, I'm Marc Sloan</span>
                                         </div>
                                     </div>
-                                    <div className="small-spacer"/>
+                                    <div className="small-medium-spacer"/>
                                     <div className="container">
                                         <div className="row justify-content-center">
                                             <div className="col-md-12 text-justified">
@@ -71,23 +81,26 @@ class IndexContainer extends Component {
                                                     target="_blank">academic</a>, <a href="https://github.com/marcsloan"
                                                                                      target="_blank">developer</a> and <a
                                                     href="https://dribbble.com/marcsloan" target="_blank">designer</a>,
-                                                    ruminating on AI and the future of the internet. I created a technology
+                                                    ruminating on AI and the future of the internet. I created a
+                                                    technology
                                                     called <a href="https://www.contextscout.com/"
-                                                              target="_blank">Scout</a> that offers real-time virtual assistance
-                                                    while browsing online. It’s over there, go ahead, you can play around with
+                                                              target="_blank">Scout</a> that offers real-time virtual
+                                                    assistance
+                                                    while browsing online. It’s over there, go ahead, you can play
+                                                    around with
                                                     it. </p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="small-spacer"/>
-                                    <div className="arrow d-none d-lg-block">
-                                        {/*Hidden on md and below*/}
-                                    </div>
-                                    <div className="medium-spacer d-none d-lg-block"/>
-                                    <div className="d-none d-lg-block">
-                                        {bottomHalfOfPage}
-                                    </div>
                                 </div>
+                            </div>
+                            <div className="small-medium-spacer"/>
+                            <div className="arrow d-none d-lg-block">
+                                {/*Hidden on md and below*/}
+                            </div>
+                            <div className="small-medium-spacer d-none d-lg-block"/>
+                            <div className="d-none d-lg-block">
+                                {bottomHalfOfPage}
                             </div>
                         </div>
                         <div className="col-lg-5">
@@ -100,7 +113,7 @@ class IndexContainer extends Component {
                 <div className="container index-container d-lg-none">
                     <div className="row">
                         <div className="index-content">
-                            <div className="medium-spacer"/>
+                            <div className="small-medium-spacer"/>
                             {bottomHalfOfPage}
                         </div>
                     </div>
