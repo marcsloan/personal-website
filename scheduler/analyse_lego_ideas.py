813521ec2d087aefceef7b677f4693abdb283b65
Marc Sloan <marc.sloan.10@ucl.ac.uk>
2021-02-18 18:23:30 +0000

    trying chrome again

--- scheduler/analyse_lego_ideas.py
+++ scheduler/analyse_lego_ideas.py
@@ -35,13 +35,14 @@ def run_lego_metrics():
     # browser = webdriver.Firefox(executable_path=path)
 
     GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+    # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
     chrome_options.binary_location = GOOGLE_CHROME_PATH
-    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     print("it worked!")
 
     # options = Options()
