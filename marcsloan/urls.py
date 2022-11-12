5fc9d0335152c46e692c8acdcefdc0a12d38d26b
Marc Sloan <marcsloan@gmail.com>
2022-11-12 13:57:41 +0000

    update urls.py

--- marcsloan/urls.py
+++ marcsloan/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.conf.urls import include
 
 import frontend.urls
