4fbae58e9b4a6e818ff55449864a05796e13eddc
Marc Sloan <hi@marcsloan.com>
2026-03-25 17:36:20 +0000

    Add STATIC_ROOT for collectstatic

--- marcsloan/settings.py
+++ marcsloan/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
